c308ab8438f886bf31d146109d2d5d406b1f3e2a
Bradley Bossard <bradleybossard@gmail.com>
2016-05-05 14:40:22 +0000

    Adding webpack-dev-server config

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,12 @@ const common = {
 
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
+/*
   devServer: {
     host: '0.0.0.0',
     port: 8080
   },
+*/
   entry: {
     app: PATHS.app
   },
@@ -29,7 +31,35 @@ const common = {
 // Default configuration. We will return this if
 // Webpack is called outside of npm.
 if(TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
 if(TARGET === 'build') {
